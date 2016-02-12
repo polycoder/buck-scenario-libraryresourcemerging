a85f1722b834f19d947a0a47ec1caf620f2e4fb9
Sean Kenny <seank@spotify.com>
2016-02-12 09:38:55 -0500

    Change to user resource dep union.

--- libs/lib1/src/main/java/com/buckbuild/scenario/lib1/subpackage1/LibOneHelper.java
+++ libs/lib1/src/main/java/com/buckbuild/scenario/lib1/subpackage1/LibOneHelper.java
@@ -7,15 +7,6 @@ import com.buckbuild.scenario.lib1.R;
 
 public class LibOneHelper {
     public String getLibTwoResStringDirectly(Context context) {
-        // NOTES
-        // - this will cause failure to build. But works when using gradle.
-    	// - this will build in buck if "resource_union_package = com.buckbuild.scenario.lib1.subpackage1"
-        //     - however, although it build the eventual android_binary built using the lib will encounter a runtime exception (the resource won't be found)
         return context.getString(R.string.lib_two_value);
-
-        // NOTES
-        // - this will work if "resource_union_package" is not set (Reference full path to resource..)
-        // - this will however break the gradle build at runtime, and is not how our libraries are organized in general (heavy use of resource merging)
-        // return context.getString(com.buckbuild.scenario.lib2.R.string.lib_two_value);
     }
 }
